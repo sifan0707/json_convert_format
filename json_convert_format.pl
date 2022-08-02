491ed0ee458b6e0444111e5a5591672145a59bad
BAIBAiBaibai <85421628+sifan0707@users.noreply.github.com>
2022-08-02 18:58:46 +0800

    Update json_convert_format.pl

--- json_convert_format.pl
+++ json_convert_format.pl
@@ -1,1 +1,10 @@
-
+#! C:\other_disk\D_disk_software\perl64 -w
+use utf8;
+binmode(STDOUT,":utf8");
+open(FH, "<GCF_016772045.1_ARS-UI_Ramb_v2.0_protein.faa") or die "file cannot open, $!";
+#将<FH>的每一行粘合在一起
+$_ = join("",<FH>);
+if($_ =~ />NP_001009191\.1.[\d\D]*?>/){
+    print $&;
+}
+close FH;
